29cc6d73d90d342c6fa2b690f917657e2a70016c
kazuki-imamaki <elm.fb.gradual@gmail.com>
2022-12-27 23:32:52 +0900

    一覧画面のフロント調整

--- resources/views/movie/want/index.blade.php
+++ resources/views/movie/want/index.blade.php
@@ -1,24 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MovieBookLog</title>
-</head>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-5 mx-auto">
+            <div class="flex flex-wrap w-full">
+                <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Bucket</h1>
+                    <div class="h-1 w-20 bg-blue-500 rounded"></div>
+                </div>
+            </div>
+        </div>
+        <div class="mb-5">
+            <a href="{{ route('want.movie.add') }} ">投稿</a>
+        </div>
+        {{-- <div class="flex flex-wrap -m-4">
+            @foreach ($wantMovies as $wantMovie)
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <div class="bg-gray-100 p-6 rounded-lg">
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                            src="https://dummyimage.com/720x400" alt="content">
+                        <h3 class="tracking-widest text-blue-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{{ $wantMovie->title }}</h2>
+                        <p class="leading-relaxed text-base mb-2">{{ $wantMovie->memo }}</p>
+                        <div class="flex">
+                            <a class="mr-2"
+                                href="{{ route('want.movie.update.index', ['movieId' => $wantMovie->id]) }} ">編集</a>
+                            <form action="{{ route('want.movie.delete', ['movieId' => $wantMovie->id]) }}"
+                                method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit">削除</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div> --}}
 
-<body>
-    <a href="{{ route('want.movie.add') }} ">投稿</a>
-    @foreach ($wantMovies as $wantMovie)
-        <p>{{ $wantMovie->title }}</p>
-        <a href="{{ route('want.movie.update.index', ['movieId' => $wantMovie->id]) }} ">編集</a>
-        <form action="{{ route('want.movie.delete', ['movieId' => $wantMovie->id]) }}" method="post">
-            @method('DELETE')
-            @csrf
-            <button type="submit">削除</button>
-        </form>
-    @endforeach
-</body>
+    </section>
 
-</html>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+
+            <div class="flex flex-wrap -m-4">
+                @foreach ($wantMovies as $wantMovie)
+                    <div class="p-4 md:w-1/3">
+                        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <img class="lg:h-48 md:h-36 w-full object-cover object-center"
+                                src="https://dummyimage.com/720x400" alt="blog">
+                            <div class="px-6 py-3">
+                                <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">MOVIE
+                                </h2>
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $wantMovie->title }}
+                                </h1>
+                                <p class="leading-relaxed mb-3 h-10">{{ $wantMovie->memo }}</p>
+                                <div class="flex">
+                                    <div>
+                                        <a class="text-blue-500 inline-flex items-center mr-2 md:mb-2 lg:mb-0"
+                                            href="{{ route('want.movie.update.index', ['movieId' => $wantMovie->id]) }} ">編集
+                                            {{-- <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
+                                            stroke-width="2" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path d="M5 12h14"></path>
+                                            <path d="M12 5l7 7-7 7"></path>
+                                        </svg> --}}
+                                        </a>
+                                    </div>
+                                    <form action="{{ route('want.movie.delete', ['movieId' => $wantMovie->id]) }}"
+                                        method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit">削除</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+</x-app-layout>
