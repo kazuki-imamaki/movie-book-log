53d39743671dc2f7d79e35986976c49b10e1d3d8
kazuki-imamaki <elm.fb.gradual@gmail.com>
2023-01-06 14:21:08 +0900

    done movieの migration、モデルを作成する

---
+++ app/Models/DoneMovie.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DoneMovie extends Model
+{
+    use HasFactory;
+
+    protected $table = 'done_movies';
+}
---
+++ database/migrations/2023_01_05_083415_create_done_movies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('done_movies', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title');
+
+            $table->string('memo')->nullable(true);
+
+            $table->string('image')->nullable(true);
+
+            $table->integer('star')->nullable(true);
+
+            $table->date('date')->nullable(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('done_movies');
+    }
+};
