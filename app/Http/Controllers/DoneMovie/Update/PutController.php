77da4217a4fbfcbe360c464d0b58988d3718ad80
kazuki-imamaki <elm.fb.gradual@gmail.com>
2023-01-11 17:59:04 +0900

    doneUpdateリクエストを統一

--- app/Http/Controllers/DoneMovie/Update/PutController.php
+++ app/Http/Controllers/DoneMovie/Update/PutController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\DoneMovie\Update;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\DoneMovie\CreateUpdateRequest;
+use App\Models\WantMovie;
 
 class PutController extends Controller
 {
@@ -13,8 +14,16 @@ class PutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(CreateUpdateRequest $request)
     {
-        //
+        // dd($request);
+        $movie = WantMovie::where('id', $request->id())->firstOrFail();
+        $movie->title = $request->title();
+        $movie->memo = $request->memo();
+        $movie->image = $request->image();
+        $movie->image = str_replace("154", "342", $movie->image);
+        $movie->is_done = $request->is_done();
+        $movie->save();
+        return redirect()->route('done.movie.update.index', ['movieId' => $movie->id])->with('feedback.success', "編集しました。");
     }
 }
