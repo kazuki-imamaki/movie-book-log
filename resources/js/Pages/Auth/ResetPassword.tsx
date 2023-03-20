e0212565efcf2c758aa2538802ab1cab2be5c74c
kazuki-imamaki <elm.fb.gradual@gmail.com>
2023-03-20 20:40:30 +0900

    fix:inertiaインポート修正

--- resources/js/Pages/Auth/ResetPassword.tsx
+++ resources/js/Pages/Auth/ResetPassword.tsx
@@ -4,7 +4,7 @@ import Guest from "@/Layouts/Guest";
 import Input from "@/Components/Input";
 import Label from "@/Components/Label";
 import ValidationErrors from "@/Components/ValidationErrors";
-import { Head, useForm } from "@inertiajs/inertia-react";
+import { Head, useForm } from "@inertiajs/react";
 
 interface Props {
     token: string;
