b5359b423256a54f9b4e7035b45db845aebe6d86
kazuki-imamaki <elm.fb.gradual@gmail.com>
2023-04-14 12:35:21 +0900

    test:api/wantのテストを作成

---
+++ tests/Feature/Api/WantMovieTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\WantMovie;
+
+class WantMovieTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_Want映画情報を取得する()
+    {
+        $user = User::factory()->create();
+
+        WantMovie::factory()->create([
+            'user_id' => $user->id,
+            'is_done' => 0
+        ]);
+
+        $response = $this->actingAs($user)->get("/api/want");
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        $response
+            ->assertJsonCount(1)
+            ->assertJsonFragment([
+                'user_id' => $user->id,
+                'is_done' => 0
+            ]);
+    }
+
+    /**
+     * Test to get want movies without login.
+     *
+     * @return void
+     */
+    public function testGetWantMoviesWithoutLogin()
+    {
+        $response = $this->get('/api/want');
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect('/login');
+    }
+}
